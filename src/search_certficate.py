212138f5902aa1f74687db055414e5f22a17c127
Petr Svenda <petrsgit@gmail.com>
2019-11-14 00:14:04 +0100

    added dedicated metadata extraction from preface of reports

--- src/search_certficate.py
+++ src/search_certficate.py
@@ -11,6 +11,7 @@ from shutil import copyfile
 
 REGEXEC_SEP = '[ ,;\]”)(]'
 LINE_SEPARATOR = ' '
+#LINE_SEPARATOR = ''  # if newline is not replaced with space, long string included in matches are found
 TAG_MATCH_COUNTER = 'count'
 TAG_MATCH_MATCHES = 'matches'
 
@@ -41,7 +42,7 @@ def get_line_number(lines, line_length_compensation, match_start_index):
     return -1
 
 
-def parse_cert_file(file_name):
+def load_cert_file(file_name, limit_max_lines=-1, line_separator=LINE_SEPARATOR):
     lines = []
     was_unicode_decode_error = False
     with open(file_name, 'r') as f:
@@ -69,21 +70,50 @@ def parse_cert_file(file_name):
     # we will estimate the line for searched matches
     # => we need to known how much lines were modified (removal of eoln..)
     line_length_compensation = 1 - len(LINE_SEPARATOR)  # for removed newline and for any added separator
+    lines_included = 0
     for line in lines:
+        if limit_max_lines != -1 and lines_included >= limit_max_lines:
+            break
+
         whole_text_with_newlines += line
         line = line.replace('\n', '')
         whole_text += line
-        whole_text += LINE_SEPARATOR
+        whole_text += line_separator
+        lines_included += 1
+
+    return whole_text, whole_text_with_newlines
+
+
+def normalize_match_string(match):
+    # normalize match
+    match = match.strip()
+    match = match.rstrip(']')
+    match = match.rstrip('/')
+    match = match.rstrip(';')
+    match = match.rstrip('.')
+    match = match.rstrip('”')
+    match = match.rstrip('"')
+    match = match.rstrip(':')
+    match = match.rstrip(')')
+    match = match.rstrip('(')
+    match = match.rstrip(',')
+    match = match.replace('  ', ' ')  # two spaces into one
+
+    return match
+
+
+def parse_cert_file(file_name, search_rules, limit_max_lines=-1, line_separator=LINE_SEPARATOR):
+    whole_text, whole_text_with_newlines = load_cert_file(file_name, limit_max_lines, line_separator)
 
     # apply all rules
     items_found_all = {}
-    for rule_group in rules.keys():
+    for rule_group in search_rules.keys():
         if rule_group not in items_found_all:
             items_found_all[rule_group] = {}
 
         items_found = items_found_all[rule_group]
 
-        for rule in rules[rule_group]:
+        for rule in search_rules[rule_group]:
             rule_and_sep = rule + REGEXEC_SEP
 
             for m in re.finditer(rule_and_sep, whole_text):
@@ -92,19 +122,8 @@ def parse_cert_file(file_name):
                     items_found[rule] = {}
 
                 match = m.group()
+                match = normalize_match_string(match)
 
-                # normalize match
-                match = match.strip()
-                match = match.rstrip(']')
-                match = match.rstrip('/')
-                match = match.rstrip(';')
-                match = match.rstrip('.')
-                match = match.rstrip('”')
-                match = match.rstrip('"')
-                match = match.rstrip(':')
-                match = match.rstrip(')')
-                match = match.rstrip('(')
-                match = match.rstrip(',')
                 if match not in items_found[rule]:
                     items_found[rule][match] = {}
                     items_found[rule][match][TAG_MATCH_COUNTER] = 0
@@ -113,7 +132,7 @@ def parse_cert_file(file_name):
                 items_found[rule][match][TAG_MATCH_COUNTER] += 1
                 match_span = m.span()
                 # estimate line in original text file
-                line_number = get_line_number(lines, line_length_compensation, match_span[0])
+                # line_number = get_line_number(lines, line_length_compensation, match_span[0])
                 # start index, end index, line number
                 items_found[rule][match][TAG_MATCH_MATCHES].append([match_span[0], match_span[1], line_number])
 
@@ -168,6 +187,7 @@ def count_num_items_found(items_found_all):
                 num_items_found += 1
 
     return num_items_found
+
 
 def print_dot_graph(filter_rules_group, all_items_found, cert_id, walk_dir, out_dot_name, thick_as_occurences):
     # print dot
@@ -259,19 +279,179 @@ def save_modified_cert_file(target_file, modified_cert_file_text, is_unicode_tex
     write_file.close()
 
 
-MIN_ITEMS_FOUND = 29577
+def search_only_headers_bsi(walk_dir):
+    LINE_SEPARATOR_STRICT = ' '
+    NUM_LINES_TO_INVESTIGATE = 15
+    rules_certificate_preface = [
+#        '(BSI-DSZ-CC-.+) (?:for|For|zu) (.+) (?:from|der)',
+        '(BSI-DSZ-CC-.+) (?:for|For) (.+) from (.*)',
+        '(BSI-DSZ-CC-.+) zu (.+) der (.*)',
+    ]
+
+    all_vendors = []
+    items_found_all = {}
+    for file_name in search_files(walk_dir):
+        if not os.path.isfile(file_name):
+            continue
+        print('*** {} ***'.format(file_name))
+
+        whole_text, whole_text_with_newlines = load_cert_file(file_name, NUM_LINES_TO_INVESTIGATE, LINE_SEPARATOR_STRICT)
+
+        items_found_all[file_name] = {}
+        items_found = items_found_all[file_name]
+        for rule in rules_certificate_preface:
+            rule_and_sep = rule + REGEXEC_SEP
+
+            for m in re.finditer(rule_and_sep, whole_text):
+                # insert rule if at least one match for it was found
+                if rule not in items_found:
+                    items_found[rule] = {}
+
+                match_groups = m.groups()
+                cert_id = match_groups[0]
+                certified_item = match_groups[1]
+                vendor = match_groups[2]
+
+                items_found[rule]['cert_id'] = normalize_match_string(cert_id)
+
+                FROM_KEYWORD_LIST = [' from ', ' der ']
+                for from_keyword in FROM_KEYWORD_LIST:
+                    from_keyword_len = len(from_keyword)
+                    if certified_item.find(from_keyword) != -1:
+                        print('string **{}** detected in certified item - shall not be here, fixing...'.format(from_keyword))
+                        certified_item_first = certified_item[:certified_item.find(from_keyword)]
+                        vendor = certified_item[certified_item.find(from_keyword) + from_keyword_len:]
+                        certified_item = certified_item_first
+                        continue
+
+                items_found[rule]['certified_item'] = normalize_match_string(certified_item)
+
+                end_pos = vendor.find('\f-')
+                if end_pos == -1:
+                    end_pos = vendor.find('\fBSI')
+                if end_pos == -1:
+                    end_pos = vendor.find('Bundesamt')
+                if end_pos != -1:
+                    vendor = vendor[:end_pos]
+
+                vendor = normalize_match_string(vendor)
+                items_found[rule]['vendor'] = normalize_match_string(vendor)
+
+                if vendor not in all_vendors:
+                    all_vendors.append(vendor)
+
+    print('\n*** Detected vendors:')
+    sorted_vendors = sorted(all_vendors)
+    for vendor in sorted_vendors:
+        print(vendor)
+
+    # store results into file with fixed name and also with time appendix
+    with open("certificate_data_bsiheader.json", "w") as write_file:
+        write_file.write(json.dumps(items_found_all, indent=4, sort_keys=True))
+
+    print('\n*** Certificates without detected preface:')
+    for file_name in items_found_all.keys():
+        if len(items_found_all[file_name]) < 1:
+            print('No hits for {}'.format(file_name))
+
+    print('\n**********************************')
+
+def search_only_headers_anssi(walk_dir):
+    LINE_SEPARATOR_STRICT = ' '
+    NUM_LINES_TO_INVESTIGATE = -1
+    rules_certificate_preface = [
+        'Référence du rapport de certification(.+)Nom du produit(.+)Référence/version du produit(.*)Conformité à un profil de protection(.+)Critères d\'évaluation et version(.+)Niveau d\'évaluation(.+)Développeurs(.+)Centre d\'évaluation(.+)Accords de reconnaissance applicables',
+        'Référence du rapport de certification(.+)Nom du produit(.+)Référence/version du produit(.*)Conformité à un profil de protection(.+)Critères d’évaluation et version(.+)Niveau d’évaluation(.+)Développeurs(.+)Centre d’évaluation(.+)Accords de reconnaissance applicables',
+        'Référence du rapport de certification(.+)Nom du produit(.+)()Conformité à un profil de protection(.+)Critères d\'évaluation et version(.+)Niveau d\'évaluation(.+)Développeur(.+)Centre d\'évaluation(.+)Accords de reconnaissance applicables',
+        'Référence du rapport de certification(.+)Nom des produits(.+)Référence/version des produits(.+)Conformité à un profil de protection(.+)Critères d\'évaluation et version(.+)Niveau d\'évaluation(.+)Développeur\(s\)(.+)Centre d\'évaluation(.+)Accords de reconnaissance applicables',
+        'Certification report reference(.+)Product name(.+)Product reference(.+)Protection profile conformity(.+)Evaluation criteria and version(.+)Evaluation level(.+)Developer(?:s|\(s\))(.+)Evaluation facility(.+)Recognition arrangements',
+    ]
+
+
+    all_vendors = []
+    items_found_all = {}
+    for file_name in search_files(walk_dir):
+        if not os.path.isfile(file_name):
+            continue
+        print('*** {} ***'.format(file_name))
+
+        whole_text, whole_text_with_newlines = load_cert_file(file_name)
+
+        items_found_all[file_name] = {}
+        items_found = items_found_all[file_name]
+        for rule in rules_certificate_preface:
+            rule_and_sep = rule + REGEXEC_SEP
+
+            for m in re.finditer(rule_and_sep, whole_text):
+                # insert rule if at least one match for it was found
+                if rule not in items_found:
+                    items_found[rule] = {}
+
+                match_groups = m.groups()
+                cert_id = match_groups[0]
+                items_found[rule]['cert_id'] = normalize_match_string(cert_id)
+                certified_item = match_groups[1]
+                items_found[rule]['certified_item'] = normalize_match_string(certified_item)
+                certified_item_version = match_groups[2]
+                items_found[rule]['certified_item_version'] = normalize_match_string(certified_item_version)
+                ref_protection_profiles = match_groups[3]
+                items_found[rule]['referenced_protection_profile'] = normalize_match_string(ref_protection_profiles)
+                cc_version = match_groups[4]
+                items_found[rule]['cc_version'] = normalize_match_string(cc_version)
+                cc_security_level = match_groups[5]
+                items_found[rule]['cc_security_level'] = normalize_match_string(cc_security_level)
+                vendor = match_groups[6]
+                vendor = normalize_match_string(vendor)
+                items_found[rule]['vendor'] = normalize_match_string(vendor)
+
+                if vendor not in all_vendors:
+                    all_vendors.append(vendor)
+
+    print('\n*** Detected vendors:')
+    sorted_vendors = sorted(all_vendors)
+    for vendor in sorted_vendors:
+        print(vendor)
+
+    # store results into file with fixed name and also with time appendix
+    with open("certificate_data_anssiheader.json", "w") as write_file:
+        write_file.write(json.dumps(items_found_all, indent=4, sort_keys=True))
+
+    print('\n*** Certificates without detected preface:')
+    for file_name in items_found_all.keys():
+        if len(items_found_all[file_name]) < 1:
+            print('No hits for {}'.format(file_name))
+
+    print('\n**********************************')
+
+
+MIN_ITEMS_FOUND = 29725
 
 
 def main():
+    # change current directory to store results into results file
+    current_dir = os.getcwd()
+    os.chdir(current_dir + '\\..\\results\\')
+
     #walk_dir = 'c:\\Certs\\certs\\cc_search\\only_ic_sc\\test\\'
     #walk_dir = 'c:\\Certs\\certs\\cc_search\\only_ic_sc\\txt\\'
     #walk_dir = 'c:\\Certs\\certs\\cc_search\\20191109_icsconly_current\\'
-    walk_dir = 'c:\\Certs\\certs\\cc_search\\20191109_icsconly_currentandachived\\'
+    #walk_dir = 'c:\\Certs\\certs\\cc_search\\20191109_icsconly_currentandachived\\'
     #walk_dir = 'c:\\Certs\\certs\\cc_search\\20191109_icsconly_currentandachived_bsionly\\'
-    #walk_dir = 'c:\\Certs\\certs\\cc_search\\20191109_icsconly_currentandachived_anssionly\\'
+    walk_dir = 'c:\\Certs\\certs\\cc_search\\20191109_icsconly_currentandachived_anssionly\\'
+    #walk_dir = 'c:\\Certs\\certs\\cc_search\\20191109_icsconly_currentandachived_others\\'
     #walk_dir = 'c:\\Certs\\certs\\cc_search\\test\\'
     #walk_dir = 'c:\\Certs\\certs\\cc_search\\test2\\'
     #walk_dir = 'c:\\Certs\\certs\\cc_search\\test3\\'
+    #walk_dir = 'c:\\Certs\\certs\\cc_search\\test4\\'
+    #walk_dir = 'c:\\Certs\\certs\\cc_search\\test5\\'
+    #walk_dir = 'c:\\Certs\\certs\\cc_search\\test6\\'
+
+    search_only_headers_anssi(walk_dir)
+
+    #search_only_headers_bsi(walk_dir)
+
+
+    return
 
     all_items_found = {}
     cert_id = {}
@@ -283,7 +463,7 @@ def main():
         print('*** {} ***'.format(file_name))
 
         # parse certificate, return all matches
-        all_items_found[file_name], modified_cert_file = parse_cert_file(file_name)
+        all_items_found[file_name], modified_cert_file = parse_cert_file(file_name, rules, -1)
 
         # try to establish the certificate id of the current certificate
         cert_id[file_name], all_certid_found_count[file_name] = estimate_cert_id(all_items_found[file_name], file_name)
