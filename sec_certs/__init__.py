343f7a5ca70da7289f94fbd227896ba722e6396a
J08nY <johny@neuromancer.sk>
2020-10-09 01:07:32 +0200

    Solve freeze issues.

--- sec_certs/__init__.py
+++ sec_certs/__init__.py
@@ -1,6 +1,7 @@
 import os
 import click
 import json
+from hashlib import blake2b
 
 from flag import flag
 from flask import Flask, render_template, abort
@@ -8,35 +9,36 @@ from flask.cli import with_appcontext
 
 
 def create_app():
-	app = Flask(__name__, instance_relative_config=True)
-	app.config.from_pyfile('config.py', silent=True)
-	# ensure the instance folder exists
-	try:
-		os.makedirs(app.instance_path)
-	except OSError:
-		pass
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_pyfile("config.py", silent=True)
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-	with open("certificate_data_complete.json") as f:
-		cc_data = json.load(f)
-	
-	@app.template_global("country_to_flag")
-	def to_flag(code):
-		return flag(code)
+    with open("certificate_data_complete.json") as f:
+        loaded_cc_data = json.load(f)
+        cc_data = {blake2b(key.encode(), digest_size=20).hexdigest() : value for key, value in loaded_cc_data.items()}
+        cc_names = {key: value["csv_scan"]["cert_item_name"] for key, value in cc_data.items()}
 
-	@app.route("/")
-	def index():
-		return render_template("index.html.jinja2")
+    @app.template_global("country_to_flag")
+    def to_flag(code):
+        return flag(code)
 
-	@app.route("/cc/")
-	@app.route("/cc/<path>/")
-	def cc(path=None):
-		if path is None:
-			return render_template("cc/index.html.jinja2", certs=cc_data.keys())
-		if path in cc_data.keys():
-			return render_template("cc/entry.html.jinja2", cert=cc_data[path], name=path)
-		else:
-			return abort(404)
+    @app.route("/")
+    def index():
+        return render_template("index.html.jinja2")
 
-	return app
+    @app.route("/cc/")
+    @app.route("/cc/<hashid>/")
+    def cc(hashid=None):
+        if hashid is None:
+            return render_template("cc/index.html.jinja2", certs=cc_names)
 
+        if hashid in cc_data.keys():
+            return render_template("cc/entry.html.jinja2", cert=cc_data[hashid])
+        else:
+            return abort(404)
 
+    return app
