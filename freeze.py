233c780c892eaf5793bb131e16cb2ee05c2988d4
J08nY <johny@neuromancer.sk>
2020-10-11 16:10:55 +0200

    Add renering of SARs and SFRs.

--- freeze.py
+++ freeze.py
@@ -5,4 +5,8 @@ from sec_certs import create_app
 if __name__ == "__main__":
 	app = create_app()
 	freezer = Freezer(app)
+	@freezer.register_generator
+	def cc_entries():
+	    for i in range(1, 111):
+	        yield "cc.index", {"page": i}
 	freezer.freeze()
