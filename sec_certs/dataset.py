3806a43b0cea6cbd7e4fe719a6765dca13adb4ea
Adam Janovsky <janovsky.vm@gmail.com>
2020-10-25 08:11:55 +0100

    further advances on mockup classes, meta (csv, html) parsers introduced

--- sec_certs/dataset.py
+++ sec_certs/dataset.py
@@ -1,12 +1,17 @@
+from .meta_parser import CCMetaParser
+from .certificate import CommonCriteriaCert
+from . import constants
+
+
 class Dataset:
-    def __init__(self):
-        self.certs = {}
-        self.framework = None
-        self.data_dir = None
+    def __init__(self, certs, framework, data_dir, name='dataset name', description='dataset_description'):
+        self.certs = certs
+        self.framework = framework
+        self.data_dir = data_dir
 
         self.sha256_digest = 'not implemented'  # TODO: Implement as hash of all certs
-        self.name = 'not implemented'  # TODO: Allow for naming of the datasets
-        self.description = 'not implemented'  # Allow for descriptions of the dataset
+        self.name = name  # TODO: Allow for naming of the datasets
+        self.description = description  # Allow for descriptions of the dataset
 
     # The idea is to iterate over dataset as: `for cert in Dataset: ... `
     def __iter__(self):
@@ -38,10 +43,24 @@ class Dataset:
     def __str__(self):
         return 'Not implemented'  # TODO: Prepare some meaningful representation of the dataset
 
+    @classmethod
+    def init_from_meta(cls, data_dir, name, description):
+        pass
 
-class DatasetCC(Dataset):
-    pass
+
+class CCDataset(Dataset):
+    @classmethod
+    def init_from_meta(cls, data_dir, name, description):
+        parser = CCMetaParser()
+        records = parser.parse_meta()
+        certs = {}
+
+        for r in records:
+            cert = CommonCriteriaCert.from_dict(r)
+            certs[cert.sha256] = cert
+
+        return CCDataset(certs, constants.CertFramework.CC, data_dir, name, description)
 
 
-class DatasetFIPS(Dataset):
+class FIPSDataset(Dataset):
     pass
