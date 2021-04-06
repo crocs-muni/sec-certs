7b7e26b6f47581f2a2cf066c5f5e11567375139f
Petr Svenda <petrsgit@gmail.com>
2021-04-06 15:26:55 +0200

    conflict resolved

--- sec_certs/extract_certificates.py
+++ sec_certs/extract_certificates.py
@@ -420,11 +420,11 @@ def search_only_headers_bsi(walk_dir: Path):
                     if end_pos != -1:
                         developer = developer[:end_pos]
 
-                    items_found[constants.TAG_CERT_ID] = normalize_match_string(cert_id)
-                    items_found[constants.TAG_CERT_ITEM] = normalize_match_string(
+                    items_found[TAG_CERT_ID] = normalize_match_string(cert_id)
+                    items_found[TAG_CERT_ITEM] = normalize_match_string(
                         certified_item)
-                    items_found[constants.TAG_DEVELOPER] = normalize_match_string(developer)
-                    items_found[constants.TAG_CERT_LAB] = 'BSI'
+                    items_found[TAG_DEVELOPER] = normalize_match_string(developer)
+                    items_found[TAG_CERT_LAB] = 'BSI'
 
             #
             # Process page with more detailed certificate info
