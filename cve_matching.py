01520b87b926cd53a58abc1a1cfb1bfa8c0602d3
Adam Janovsky <janovsky.vm@gmail.com>
2021-09-21 14:04:34 +0200

    PoC GridSearch on hyperparameters

--- cve_matching.py
+++ cve_matching.py
@@ -3,20 +3,21 @@ import datetime
 
 from sec_certs.dataset.common_criteria import CCDataset
 from sec_certs.dataset.cve import CVEDataset
-from sec_certs.model.evaluation import get_validation_dgsts, binarize_labels, get_y_true, evaluate_classifier
+from sec_certs.model.evaluation import get_validation_dgsts, get_y_true,compute_precision
 from sec_certs.model.cve_matching import VulnClassifier
+from sklearn.metrics import make_scorer
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import PredefinedSplit
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
     start = datetime.datetime.now()
 
-    validation_dgsts = get_validation_dgsts(
-        '/Users/adam/phd/projects/certificates/datasets/cve_matching_valid_test_split/validation_set.json')
-    cc_dset = CCDataset.from_json(
-        '/Users/adam/phd/projects/certificates/datasets/cc_full_dataset/CommonCriteria_dataset.json')
+    validation_dgsts = get_validation_dgsts('/Users/adam/phd/projects/certificates/datasets/cve_matching_valid_test_split/validation_set.json')
+    cc_dset = CCDataset.from_json('/Users/adam/phd/projects/certificates/datasets/cc_full_dataset/CommonCriteria_dataset.json')
     validation_certs = [x for x in cc_dset if x.dgst in validation_dgsts]
-
+    validation_certs = [x for x in validation_certs if x.heuristics.related_cves]
     keywords = cc_dset.generate_cert_name_keywords()
 
     cve_dset: CVEDataset = CVEDataset.from_json(cc_dset.cve_dataset_path)
@@ -25,11 +26,21 @@ def main():
     classifier = VulnClassifier(keywords)
     classifier.fit(vulnerabilities)
 
+    scorer = make_scorer(compute_precision)
+
+    grid_search_dict = {
+        'cutoff_distance': [0.4, 0.7, 1],
+        'n_tokens': [20, 25, 30]
+    }
+
     cert_names = [x.name for x in validation_certs]
-    y_pred = binarize_labels(cve_dset, classifier.predict(cert_names))
-    y_true = binarize_labels(cve_dset, get_y_true(validation_certs))
-    precision = evaluate_classifier(y_pred, y_true)
-    print(f'Precision: {precision}')
+    validation_fold = [-1] * len(vulnerabilities) + [0] * len(cert_names)
+    ps = PredefinedSplit(validation_fold)
+    full_dataset = vulnerabilities + cert_names
+
+    y_true = get_y_true(validation_certs)
+    clf = GridSearchCV(classifier, param_grid=grid_search_dict, scoring=scorer, verbose=4, cv=ps, refit=False)
+    clf.fit(full_dataset, [0] * len(vulnerabilities) + y_true)
 
     end = datetime.datetime.now()
     print(f'The computation took {(end - start)} seconds.')
