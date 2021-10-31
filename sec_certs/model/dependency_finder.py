c3e2523865aa4d50cacf06696a7a6104d4213726
GeogeFI <jirimichalik1@gmail.com>
2021-10-31 18:25:53 +0100

    feat: Created DependencyFinder class - just structure

---
+++ sec_certs/model/dependency_finder.py
@@ -0,0 +1,11 @@
+from typing import List, Set
+
+
+class DependencyFinder:
+    @staticmethod
+    def fit(certificates: List) -> None:
+        pass
+
+    @staticmethod
+    def predict(cert) -> Set:
+        pass
