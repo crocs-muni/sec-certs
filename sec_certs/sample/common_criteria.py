d5440705d7228985c182637e4f33677d422c8904
Adam Janovsky <janovsky.vm@gmail.com>
2021-11-01 07:17:45 +0100

    Rename heuristics to CCHeuristics
    Just in case, problem is when deserializing multiple classes with same name from json.
    For this purpose, we explicitly differentiate CCHeuristics and FIPSHeuristics

--- sec_certs/sample/common_criteria.py
+++ sec_certs/sample/common_criteria.py
@@ -190,7 +190,7 @@ class CommonCriteriaCert(Certificate, ComplexSerializableType):
             return processed if (processed := self.processed_cert_id) else self.keywords_cert_id
 
     @dataclass
-    class Heuristics(ComplexSerializableType):
+    class CCHeuristics(ComplexSerializableType):
         extracted_versions: List[str] = field(default=None)
         cpe_matches: Optional[Set[str]] = field(default=None)
         labeled: bool = field(default=False)
@@ -224,7 +224,7 @@ class CommonCriteriaCert(Certificate, ComplexSerializableType):
                  maintainance_updates: Set[MaintainanceReport],
                  state: Optional[InternalState],
                  pdf_data: Optional[PdfData],
-                 heuristics: Optional[Heuristics]):
+                 heuristics: Optional[CCHeuristics]):
         super().__init__()
 
         self.status = status
@@ -251,7 +251,7 @@ class CommonCriteriaCert(Certificate, ComplexSerializableType):
         self.pdf_data = pdf_data
 
         if heuristics is None:
-            heuristics = self.Heuristics()
+            heuristics = self.CCHeuristics()
         self.heuristics = heuristics
 
     @property
