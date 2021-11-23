46002cde7a6b96f9321b3b9a08afe7ff35583237
Adam Janovsky <janovsky.vm@gmail.com>
2021-11-23 18:10:21 +0100

    fix cc main. update pandas serialization

--- sec_certs/sample/cc_maintenance_update.py
+++ sec_certs/sample/cc_maintenance_update.py
@@ -33,6 +33,10 @@ class CommonCriteriaMaintenanceUpdate(CommonCriteriaCert, ComplexSerializableTyp
     def dgst(self):
         return 'cert_' + self.related_cert_digest + '_update_' + helpers.get_first_16_bytes_sha256(self.name)
 
+    @property
+    def pandas_tuple(self) -> Tuple:
+        return tuple([getattr(self, x) for x in CommonCriteriaMaintenanceUpdate.pandas_columns])
+
     @classmethod
     def from_dict(cls, dct: Dict) -> 'CommonCriteriaMaintenanceUpdate':
         dct.pop('dgst')
@@ -42,6 +46,3 @@ class CommonCriteriaMaintenanceUpdate(CommonCriteriaCert, ComplexSerializableTyp
     def get_updates_from_cc_cert(cls, cert: CommonCriteriaCert):
         return [cls(x.maintainance_title, x.maintainance_report_link, x.maintainance_st_link,
                     None, None, None, cert.dgst, x.maintainance_date) for x in cert.maintainance_updates]
-
-    def to_pandas_tuple(self) -> Tuple:
-        return tuple([getattr(self, x) for x in CommonCriteriaMaintenanceUpdate.pandas_columns])
