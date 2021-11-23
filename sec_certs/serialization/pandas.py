2af6550130e75bce5b1eb78837daba3305aaeba5
Adam Janovsky <janovsky.vm@gmail.com>
2021-11-23 14:18:46 +0100

    pandas serialization module

---
+++ sec_certs/serialization/pandas.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+
+class PandasSerializableType(ABC):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    @property
+    @abstractmethod
+    def pandas_tuple(self):
+        raise NotImplementedError('Not meant to be implemented')
+
+    @property
+    @abstractmethod
+    def pandas_columns(self):
+        raise NotImplementedError('Not meant to be implemented')
