57fdc1cb6d83ce13c4436778a2f9a5003d56647d
J08nY <johny@neuromancer.sk>
2022-01-27 11:45:08 +0100

    Better fix for date loading in CC.

--- sec_certs/sample/common_criteria.py
+++ sec_certs/sample/common_criteria.py
@@ -357,8 +357,8 @@ class CommonCriteriaCert(Certificate, PandasSerializableType, ComplexSerializabl
         new_dct = dct.copy()
         new_dct["maintenance_updates"] = set(dct["maintenance_updates"])
         new_dct["protection_profiles"] = set(dct["protection_profiles"])
-        new_dct["not_valid_before"] = date.fromisoformat(dct["not_valid_before"]) if dct["not_valid_before"] else None
-        new_dct["not_valid_after"] = date.fromisoformat(dct["not_valid_after"]) if dct["not_valid_after"] else None
+        new_dct["not_valid_before"] = date.fromisoformat(dct["not_valid_before"]) if isinstance(dct["not_valid_before"], str) else dct["not_valid_before"]
+        new_dct["not_valid_after"] = date.fromisoformat(dct["not_valid_after"]) if isinstance(dct["not_valid_after"], str) else dct["not_valid_after"]
         return super(cls, CommonCriteriaCert).from_dict(new_dct)
 
     @classmethod
