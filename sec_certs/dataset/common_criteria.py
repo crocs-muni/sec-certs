bd9b88a5ae267124845de0fe9d0d45f8059f693f
GeogeFI <jirimichalik1@gmail.com>
2022-04-09 15:17:16 +0200

    test: Added tests for dependecy vulnerabilities

--- sec_certs/dataset/common_criteria.py
+++ sec_certs/dataset/common_criteria.py
@@ -691,6 +691,10 @@ class CCDataset(Dataset[CommonCriteriaCert], ComplexSerializableType):
 
         for dgst in self.certs:
             dependency_cve = cve_dependency_finder.predict_single_cert(dgst)
+
+            if dependency_cve.direct_dependency_cves:
+                print(self.certs[dgst])
+
             self.certs[dgst].heuristics.direct_dependency_cves = dependency_cve.direct_dependency_cves
             self.certs[dgst].heuristics.indirect_dependency_cves = dependency_cve.indirect_dependency_cves
 
