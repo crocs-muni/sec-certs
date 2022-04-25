5ede8f4dc9b3322b04480b3f9caa99c382fa1042
Adam Janovsky <janovsky.vm@gmail.com>
2022-04-25 18:11:07 +0200

    add notebooks to docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-from typing import List
 
 # -- Project information -----------------------------------------------------
 
@@ -31,7 +30,7 @@ release = "0.0.4"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions: List[str] = ["myst_parser"]
+extensions = ["myst_nb"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
--- docs/index.md
+++ docs/index.md
@@ -1,10 +1,19 @@
 # Sec-certs documentation
 
+## Notebooks
+
+```{toctree}
+:caption: Notebook examples
+notebooks/dataset.ipynb
+```
+
+## GitHub artifacts
+
 ```{toctree}
 :maxdepth: 2
+:caption: GitHub artifacts
 readme.md
 contributing.md
 code_of_conduct.md
 license.md
 ```
-
