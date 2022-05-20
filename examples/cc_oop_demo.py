10b3b57c8df3e467df2f8fbc617c25a6c0d578d5
Adam Janovsky <janovsky.vm@gmail.com>
2022-05-20 10:37:09 +0200

    simplify FIPS,CC examples

--- examples/cc_oop_demo.py
+++ examples/cc_oop_demo.py
@@ -1,6 +1,5 @@
 import logging
 from datetime import datetime
-from pathlib import Path
 
 from sec_certs.config.configuration import config
 from sec_certs.dataset.common_criteria import CCDataset
@@ -17,39 +16,25 @@ def main():
     logging.basicConfig(level=logging.INFO, handlers=[file_handler, stream_handler])
     start = datetime.now()
 
-    # Create empty dataset
-    dset = CCDataset({}, Path("./debug_dataset"), "cc_full_dataset", "sample dataset description")
+    # Full processing pipeline of Common Criteria dataset
+    dset = CCDataset()
+    dset.get_certs_from_web()
+    dset.process_protection_profiles()
+    dset.download_all_pdfs()
+    dset.convert_all_pdfs()
+    dset.analyze_certificates()  # calls dset._extract_data() and dset._compute_heuristics()
 
-    # Load metadata for certificates from CSV and HTML sources
-    dset.get_certs_from_web(to_download=True)
+    # Other useful API below
 
     # explicitly dump to json
-    dset.to_json(dset.json_path)
-
-    # Retrieve protection profile IDs
-    dset.process_protection_profiles()
+    # dset.to_json(dset.json_path)
 
     # Load dataset from JSON
-    new_dset = CCDataset.from_json("./debug_dataset/cc_full_dataset.json")
-    assert dset == new_dset
-
-    # Download pdfs and update json
-    dset.download_all_pdfs()
-
-    # Convert pdfs to text and update json
-    dset.convert_all_pdfs()
-
-    # Extract data from txt files and update json
-    dset._extract_data()
+    # new_dset = CCDataset.from_json("./debug_dataset/cc_full_dataset.json")
+    # assert dset == new_dset
 
     # transform to pandas DataFrame
     # df = dset.to_pandas()
-
-    # Compute heuristics on the dataset
-    dset._compute_heuristics()
-
-    # Compute related cves
-    # dset.compute_related_cves()
 
     end = datetime.now()
     logger.info(f"The computation took {(end-start)} seconds.")
