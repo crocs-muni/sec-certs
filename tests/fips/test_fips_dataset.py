98c4f06c6ced66beab6f0c576542acbf15212416
J08nY <johny@neuromancer.sk>
2023-04-11 17:30:10 +0200

    Move FIPS test fixtures to conftest and share them.

--- tests/fips/test_fips_dataset.py
+++ tests/fips/test_fips_dataset.py
@@ -4,30 +4,19 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 
 import pytest
-import tests.data.fips.dataset
 
 from sec_certs import constants
 from sec_certs.dataset.fips import FIPSDataset
 from sec_certs.sample.fips import FIPSCertificate
 
 
-@pytest.fixture(scope="module")
-def data_dir() -> Path:
-    return Path(tests.data.fips.dataset.__path__[0])
-
-
-@pytest.fixture
-def toy_dataset(data_dir: Path) -> FIPSDataset:
-    return FIPSDataset.from_json(data_dir / "toy_dataset.json")
-
-
-def test_dataset_to_json(toy_dataset: FIPSDataset, data_dir: Path, tmp_path: Path):
+def test_dataset_to_json(toy_dataset: FIPSDataset, dataset_data_dir: Path, tmp_path: Path):
     toy_dataset.to_json(tmp_path / "dset.json")
 
     with (tmp_path / "dset.json").open("r") as handle:
         data = json.load(handle)
 
-    with (data_dir / "toy_dataset.json").open("r") as handle:
+    with (dataset_data_dir / "toy_dataset.json").open("r") as handle:
         template_data = json.load(handle)
 
     del data["timestamp"]
@@ -35,8 +24,8 @@ def test_dataset_to_json(toy_dataset: FIPSDataset, data_dir: Path, tmp_path: Pat
     assert data == template_data
 
 
-def test_dataset_from_json(toy_dataset: FIPSDataset, data_dir: Path):
-    assert toy_dataset == FIPSDataset.from_json(data_dir / "toy_dataset.json")
+def test_dataset_from_json(toy_dataset: FIPSDataset, dataset_data_dir: Path):
+    assert toy_dataset == FIPSDataset.from_json(dataset_data_dir / "toy_dataset.json")
 
 
 def test_build_empty_dataset():
@@ -49,15 +38,19 @@ def test_build_empty_dataset():
     assert not dset.state.certs_analyzed
 
 
-def test_build_dataset(data_dir: Path, toy_dataset: FIPSDataset):
+def test_build_dataset(dataset_data_dir: Path, toy_dataset: FIPSDataset):
     with TemporaryDirectory() as tmp_dir:
         dataset_path = Path(tmp_dir)
         (dataset_path / "web").mkdir()
-        shutil.copyfile(data_dir / "fips_modules_active.html", dataset_path / "web" / "fips_modules_active.html")
         shutil.copyfile(
-            data_dir / "fips_modules_historical.html", dataset_path / "web" / "fips_modules_historical.html"
+            dataset_data_dir / "fips_modules_active.html", dataset_path / "web" / "fips_modules_active.html"
         )
-        shutil.copyfile(data_dir / "fips_modules_revoked.html", dataset_path / "web" / "fips_modules_revoked.html")
+        shutil.copyfile(
+            dataset_data_dir / "fips_modules_historical.html", dataset_path / "web" / "fips_modules_historical.html"
+        )
+        shutil.copyfile(
+            dataset_data_dir / "fips_modules_revoked.html", dataset_path / "web" / "fips_modules_revoked.html"
+        )
 
         dset = FIPSDataset(root_dir=dataset_path)
         dset.get_certs_from_web(to_download=False)
@@ -81,7 +74,7 @@ def test_download_meta_html_files():
         assert (dset.web_dir / "fips_modules_revoked.html").stat().st_size > constants.MIN_FIPS_HTML_SIZE
 
 
-def test_download_and_convert_artifacts(toy_dataset: FIPSDataset, data_dir: Path):
+def test_download_and_convert_artifacts(toy_dataset: FIPSDataset, dataset_data_dir: Path):
     crt = toy_dataset["184097a88a9b4ad9"]
     toy_dataset.certs = {crt.dgst: crt}
     with TemporaryDirectory() as tmp_dir:
@@ -98,7 +91,7 @@ def test_download_and_convert_artifacts(toy_dataset: FIPSDataset, data_dir: Path
     assert crt.state.policy_pdf_hash == "36b63890182f0aed29b305a0b4acc0d70b657262516f4be69138c70c2abdb1f1"
     assert crt.state.policy_txt_path.exists()
 
-    template_policy_txt_path = data_dir / "template_policy_184097a88a9b4ad9.txt"
+    template_policy_txt_path = dataset_data_dir / "template_policy_184097a88a9b4ad9.txt"
     assert abs(crt.state.policy_txt_path.stat().st_size - template_policy_txt_path.stat().st_size) < 1000
 
 
