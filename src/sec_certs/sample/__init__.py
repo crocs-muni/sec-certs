c711422b9521674489923452391ca9bce6b7b9b4
J08nY <johny@neuromancer.sk>
2023-04-17 14:10:40 +0200

    Revert "Ditch the __init__ package imports."
    This reverts commit 89b3d880088b5c30fa10036f280e73b1c1aee05e.

--- src/sec_certs/sample/__init__.py
+++ src/sec_certs/sample/__init__.py
@@ -1,3 +1,34 @@
 """This package holds mostly data objects of primary interest (Common Criteria, FIPS), or assisting objects
 like CPE, CVE, etc. The objects mostly hold data and allow for serialization, but can also perform some basic transformations.
 """
+
+from sec_certs.sample.cc import CCCertificate
+from sec_certs.sample.cc_certificate_id import CertificateId
+from sec_certs.sample.cc_maintenance_update import CCMaintenanceUpdate
+from sec_certs.sample.cpe import CPE, CPEConfiguration, cached_cpe
+from sec_certs.sample.cve import CVE
+from sec_certs.sample.fips import FIPSCertificate
+from sec_certs.sample.fips_algorithm import FIPSAlgorithm
+from sec_certs.sample.fips_iut import IUTEntry, IUTSnapshot
+from sec_certs.sample.fips_mip import MIPEntry, MIPSnapshot, MIPStatus
+from sec_certs.sample.protection_profile import ProtectionProfile
+from sec_certs.sample.sar import SAR
+
+__all__ = [
+    "CertificateId",
+    "CCMaintenanceUpdate",
+    "CCCertificate",
+    "CPE",
+    "CPEConfiguration",
+    "cached_cpe",
+    "CVE",
+    "FIPSCertificate",
+    "FIPSAlgorithm",
+    "IUTEntry",
+    "IUTSnapshot",
+    "MIPEntry",
+    "MIPSnapshot",
+    "MIPStatus",
+    "ProtectionProfile",
+    "SAR",
+]
