54ec633a646a356786bbcbfc50aefd14f8edba98
J08nY <johny@neuromancer.sk>
2023-04-25 13:34:02 +0200

    Expose CPE match dataset.

--- sec_certs_page/vuln/views.py
+++ sec_certs_page/vuln/views.py
@@ -61,29 +61,29 @@ def cve(cve_id):
     return render_template("vuln/cve.html.jinja2", cve=load(cve_doc), cc_certs=cc_certs, fips_certs=fips_certs)
 
 
-@vuln.route("/cpe/cpe.json")
-def cpe_dset():
-    dset_path = Path(current_app.instance_path) / current_app.config["DATASET_PATH_CPE"]
+@vuln.route("/cpe/cpe_match.json")
+def cpe_match_dset():
+    dset_path = Path(current_app.instance_path) / current_app.config["DATASET_PATH_CPE_MATCH"]
     if not dset_path.is_file():
         return abort(404)
     return send_file(
         dset_path,
         as_attachment=True,
         mimetype="application/json",
-        download_name="cpe.json",
+        download_name="cpe_match.json",
     )
 
 
-@vuln.route("/cpe/cpe.json.gz")
-def cpe_dset_gz():
-    dset_path = Path(current_app.instance_path) / current_app.config["DATASET_PATH_CPE_COMPRESSED"]
+@vuln.route("/cpe/cpe_match.json.gz")
+def cpe_match_dset_gz():
+    dset_path = Path(current_app.instance_path) / current_app.config["DATASET_PATH_CPE_MATCH_COMPRESSED"]
     if not dset_path.is_file():
         return abort(404)
     return send_file(
         dset_path,
         as_attachment=True,
         mimetype="application/json",
-        download_name="cpe.json.gz",
+        download_name="cpe_match.json.gz",
     )
 
 
@@ -111,6 +111,32 @@ def cpe(cpe_id):
     )
 
 
+@vuln.route("/cpe/cpe.json")
+def cpe_dset():
+    dset_path = Path(current_app.instance_path) / current_app.config["DATASET_PATH_CPE"]
+    if not dset_path.is_file():
+        return abort(404)
+    return send_file(
+        dset_path,
+        as_attachment=True,
+        mimetype="application/json",
+        download_name="cpe.json",
+    )
+
+
+@vuln.route("/cpe/cpe.json.gz")
+def cpe_dset_gz():
+    dset_path = Path(current_app.instance_path) / current_app.config["DATASET_PATH_CPE_COMPRESSED"]
+    if not dset_path.is_file():
+        return abort(404)
+    return send_file(
+        dset_path,
+        as_attachment=True,
+        mimetype="application/json",
+        download_name="cpe.json.gz",
+    )
+
+
 @sitemap.register_generator
 def sitemap_urls():
     yield "vuln.index", {}
@@ -118,6 +144,8 @@ def sitemap_urls():
     yield "vuln.cve_dset_gz", {}
     yield "vuln.cpe_dset", {}
     yield "vuln.cpe_dset_gz", {}
+    yield "vuln.cpe_match_dset", {}
+    yield "vuln.cpe_match_dset_gz", {}
     for doc in mongo.db.cve.find({}, {"_id": 1}):
         yield "vuln.cve", {"cve_id": doc["_id"]}
     for doc in mongo.db.cpe.find({}, {"_id": 1}):
