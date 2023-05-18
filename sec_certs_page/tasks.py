d5db045321016f5dc5c067a2e9698b054ed4d487
J08nY <johny@neuromancer.sk>
2023-05-18 13:56:02 +0200

    Make sure that weekly updates run.

--- sec_certs_page/tasks.py
+++ sec_certs_page/tasks.py
@@ -8,14 +8,14 @@ from .notifications.tasks import cleanup_subscriptions
 from .vuln.tasks import update_cpe_data, update_cpe_match_data, update_cve_data
 
 
-@dramatiq.actor(periodic=cron("@weekly"))
+@dramatiq.actor(periodic=cron("0 12 * * 1"))  # Monday at 12
 def run_updates_weekly() -> None:  # pragma: no cover
     (
         update_cc_data.message_with_options(pipe_ignore=True) | update_fips_data.message_with_options(pipe_ignore=True)
     ).run()
 
 
-@dramatiq.actor(periodic=cron("@daily"))
+@dramatiq.actor(periodic=cron("0 0 * * *"))  # Every day at midnight
 def run_updates_daily() -> None:  # pragma: no cover
     (
         cleanup_subscriptions.message_with_options(pipe_ignore=True)
