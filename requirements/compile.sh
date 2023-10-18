d67361d2486279886141f911e5d4b2e0f2cdd0a9
adamjanovsky <janovsky.vm@gmail.com>
2023-10-18 17:08:22 +0200

    introduce all_requirements.txt

--- requirements/compile.sh
+++ requirements/compile.sh
@@ -2,6 +2,7 @@
 
 # See CONTRIBUTING.md for description
 
-pip-compile --no-header --resolver=backtracking --output-file=requirements.txt ./../pyproject.toml
-pip-compile --no-header --resolver=backtracking --extra dev -o dev_requirements.txt ./../pyproject.toml
-pip-compile --no-header --resolver=backtracking --extra test -o test_requirements.txt ./../pyproject.toml
+pip-compile --no-header -o requirements.txt ./../pyproject.toml
+pip-compile --no-header --extra dev -o dev_requirements.txt ./../pyproject.toml
+pip-compile --no-header --extra test -o test_requirements.txt ./../pyproject.toml
+pip-compile --no-header --extra dev --extra test -o all_requirements.txt ./../pyproject.toml
