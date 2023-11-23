258bed5f65e7683b4921d319bda545a4286a1202
Adam Janovsky <janovsky.vm@gmail.com>
2023-11-23 09:49:45 +0100

    bump requirements

--- requirements/compile.sh
+++ requirements/compile.sh
@@ -2,7 +2,7 @@
 
 # See CONTRIBUTING.md for description
 
-pip-compile --no-header --output-file=requirements.txt ./../pyproject.toml
+pip-compile --no-header -o requirements.txt ./../pyproject.toml
 pip-compile --no-header --extra dev -o dev_requirements.txt ./../pyproject.toml
 pip-compile --no-header --extra test -o test_requirements.txt ./../pyproject.toml
 pip-compile --no-header --extra nlp -o nlp_requirements.txt ./../pyproject.toml
