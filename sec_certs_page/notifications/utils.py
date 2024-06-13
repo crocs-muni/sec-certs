d8b2647e025901985d87e2abf8bc93bb3aa4f39d
J08nY <johny@neuromancer.sk>
2024-06-13 16:06:38 +0200

    Fix base64 in email.

--- sec_certs_page/notifications/utils.py
+++ sec_certs_page/notifications/utils.py
@@ -24,5 +24,6 @@ class Message(FlaskMessage):
     def _mimetext(self, text, subtype="plain"):
         res = super()._mimetext(text, subtype)
         if subtype == "html":
+            del res["Content-Transfer-Encoding"]
             encode_base64(res)
         return res
