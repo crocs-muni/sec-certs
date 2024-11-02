fa1d7b8a5e21870ec3f395cbcddc86341de9fb75
J08nY <johny@neuromancer.sk>
2024-11-02 14:00:04 +0100

    Enable dark mode on analysis plots.

--- sec_certs_page/jinja.py
+++ sec_certs_page/jinja.py
@@ -1,11 +1,13 @@
 import re
 import time
 from datetime import date, datetime
+from os.path import join
 
 import sentry_sdk
 from flag import flag
 from flask import current_app, request
 from flask_principal import Permission, RoleNeed
+from markupsafe import Markup
 from sec_certs.utils.extract import flatten_matches as dict_flatten
 from sentry_sdk.utils import get_default_release
 
@@ -90,6 +92,25 @@ def endpoint():
 @app.template_global("event_navbar")
 def event_navbar():
     return runtime_config.get("EVENT_NAVBAR")
+
+
+@app.template_global("include_static")
+def include_static(filename):
+    bp = current_app.blueprints.get(request.blueprint)
+    if bp is not None and bp.static_folder is not None:
+        try:
+            with open(join(bp.static_folder, filename), "r") as f:
+                return Markup(f.read())
+        except FileNotFoundError:
+            return None
+    elif current_app.static_folder is not None:
+        try:
+            with open(join(current_app.static_folder, filename), "r") as f:
+                return Markup(f.read())
+        except FileNotFoundError:
+            return None
+    else:
+        return None
 
 
 release = get_default_release()
