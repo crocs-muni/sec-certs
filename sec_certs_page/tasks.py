9b83858bbd84417118f9abd52670d140817be8c8
J08nY <johny@neuromancer.sk>
2024-11-22 13:54:23 +0100

    Add task for index optimization.

--- sec_certs_page/tasks.py
+++ sec_certs_page/tasks.py
@@ -1,6 +1,7 @@
 import dramatiq
 from periodiq import cron
 
+from . import whoosh_index
 from .cc.tasks import update_data as update_cc_data
 from .fips.tasks import update_data as update_fips_data
 from .fips.tasks import update_iut_data, update_mip_data
@@ -8,7 +9,9 @@ from .notifications.tasks import cleanup_subscriptions
 from .vuln.tasks import update_cpe_data, update_cpe_match_data, update_cve_data
 
 
-@dramatiq.actor(actor_name="run_updates_weekly", queue_name="updates", periodic=cron("0 12 * * 1"))  # Monday at 12
+@dramatiq.actor(
+    actor_name="run_updates_weekly", queue_name="updates", periodic=cron("0 12 * * 1")
+)  # Every Monday at 12:00
 def run_updates_weekly() -> None:  # pragma: no cover
     (
         update_cc_data.message_with_options(pipe_ignore=True) | update_fips_data.message_with_options(pipe_ignore=True)
@@ -27,3 +30,8 @@ def run_updates_daily() -> None:  # pragma: no cover
         | update_iut_data.message_with_options(pipe_ignore=True)
         | update_mip_data.message_with_options(pipe_ignore=True)
     ).run()
+
+
+@dramatiq.actor(actor_name="optimize_index")
+def optimize_index() -> None:
+    whoosh_index.optimize()
