1e96600919765b54329bb3a6b949670bcf47450c
Adam Janovsky <adam.janovsky@invasys.com>
2025-01-27 15:53:09 +0100

    bump deps, foster auto updates

--- requirements/compile.sh
+++ requirements/compile.sh
@@ -2,8 +2,8 @@
 
 # See CONTRIBUTING.md for description
 
-pip-compile --no-header -o requirements.txt ./../pyproject.toml
-pip-compile --no-header --extra dev -o dev_requirements.txt ./../pyproject.toml
-pip-compile --no-header --extra test -o test_requirements.txt ./../pyproject.toml
-pip-compile --no-header --extra nlp -o nlp_requirements.txt ./../pyproject.toml
-pip-compile --no-header --extra dev --extra test --extra nlp -o all_requirements.txt ./../pyproject.toml
+pip-compile --upgrade --no-header -o requirements.txt ./../pyproject.toml
+pip-compile --upgrade --no-header --extra dev -o dev_requirements.txt ./../pyproject.toml
+pip-compile --upgrade --no-header --extra test -o test_requirements.txt ./../pyproject.toml
+pip-compile --upgrade --no-header --extra nlp -o nlp_requirements.txt ./../pyproject.toml
+pip-compile --upgrade --no-header --extra dev --extra test --extra nlp -o all_requirements.txt ./../pyproject.toml
