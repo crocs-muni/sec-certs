4b5754c75348ca8fadfacaab014f00d8715dfffa
J08nY <johny@neuromancer.sk>
2025-02-05 16:43:48 +0100

    Unify PP search.

--- sec_certs_page/pp/views.py
+++ sec_certs_page/pp/views.py
@@ -1,5 +1,4 @@
 import random
-import re
 from operator import itemgetter
 from pathlib import Path
 
@@ -7,14 +6,21 @@ import pymongo
 import sentry_sdk
 from flask import abort, current_app, redirect, render_template, request, send_file, url_for
 from flask_breadcrumbs import register_breadcrumb
-from werkzeug.exceptions import BadRequest
 
 from .. import mongo, sitemap
-from ..cc import cc_categories
+from ..cc import cc_schemes
 from ..common.feed import Feed
 from ..common.objformats import StorageFormat, load
-from ..common.views import Pagination, entry_download_files, send_json_attachment
+from ..common.views import (
+    entry_download_files,
+    entry_download_profile_pdf,
+    entry_download_profile_txt,
+    entry_download_report_pdf,
+    entry_download_report_txt,
+    send_json_attachment,
+)
 from . import pp
+from .search import PPBasicSearch
 from .tasks import PPRenderer
 
 
@@ -45,7 +51,7 @@ def dataset():
     )
 
 
-@pp.route("/cc.tar.gz")
+@pp.route("/pp.tar.gz")
 def dataset_archive():
     """Protection Profile dataset archive API endpoint."""
     archive_path = Path(current_app.instance_path) / current_app.config["DATASET_PATH_PP_ARCHIVE"]
@@ -59,115 +65,28 @@ def dataset_archive():
     )
 
 
-# TODO: Unify PP search with the rest to enable full text search
-def select_certs(q, cat, status, sort):
-    categories = cc_categories.copy()
-    query = {}
-    projection = {
-        "_id": 1,
-        "web_data.name": 1,
-        "web_data.status": 1,
-        "web_data.not_valid_before": 1,
-        "web_data.not_valid_after": 1,
-        "web_data.category": 1,
-    }
-
-    if q is not None and q != "":
-        projection["score"] = {"$meta": "textScore"}  # type: ignore
-        query["$text"] = {"$search": q}
-
-    if cat is not None:
-        selected_cats = []
-        for name, category in categories.items():
-            if category["id"] in cat:
-                selected_cats.append(name)
-                category["selected"] = True
-            else:
-                category["selected"] = False
-        query["web_data.category"] = {"$in": selected_cats}
-    else:
-        for category in categories.values():
-            category["selected"] = True
-
-    if status is not None and status != "any":
-        query["web_data.status"] = status
-
-    cursor = mongo.db.pp.find(query, projection)
-    count = mongo.db.pp.count_documents(query)
-
-    if sort == "match" and q is not None and q != "":
-        cursor.sort(
-            [
-                ("score", {"$meta": "textScore"}),
-                ("web_data.name", pymongo.ASCENDING),
-            ]
-        )
-    elif sort == "cert_date":
-        cursor.sort([("web_data.not_valid_before", pymongo.ASCENDING)])
-    elif sort == "archive_date":
-        cursor.sort([("web_data.not_valid_after", pymongo.ASCENDING)])
-    else:
-        cursor.sort([("web_data.name", pymongo.ASCENDING)])
-
-    return cursor, categories, count
-
-
-def process_search(req, callback=None):
-    try:
-        page = int(req.args.get("page", 1))
-    except ValueError:
-        raise BadRequest(description="Invalid page number.")
-    q = req.args.get("q", None)
-    cat = req.args.get("cat", None)
-    status = req.args.get("status", "any")
-    if status not in ("any", "active", "archived"):
-        raise BadRequest(description="Invalid status.")
-    sort = req.args.get("sort", "match")
-    if sort not in ("match", "name", "cert_date", "archive_date"):
-        raise BadRequest(description="Invalid sort.")
-
-    cursor, categories, count = select_certs(q, cat, status, sort)
-
-    per_page = current_app.config["SEARCH_ITEMS_PER_PAGE"]
-    pagination = Pagination(
-        page=page,
-        per_page=per_page,
-        search=True,
-        found=count,
-        total=mongo.db.pp.count_documents({}),
-        css_framework="bootstrap5",
-        alignment="center",
-        url_callback=callback,
-    )
-    return {
-        "pagination": pagination,
-        "profiles": list(map(load, cursor[(page - 1) * per_page : page * per_page])),
-        "categories": categories,
-        "q": q,
-        "page": page,
-        "status": status,
-        "sort": sort,
-    }
-
-
 @pp.route("/search/")
 @register_breadcrumb(pp, ".search", "Search")
 def search():
-    res = process_search(request)
+    """Protection Profile search."""
+    res = PPBasicSearch.process_search(request)
     return render_template(
-        "pp/search.html.jinja2",
+        "pp/search/index.html.jinja2",
         **res,
-        title=f"Protection Profile [{res['q']}] ({res['page']}) | sec-certs.org",
+        schemes=cc_schemes,
+        title=f"Protection Profile [{res['q'] if res['q'] else ''}] ({res['page']}) | sec-certs.org",
     )
 
 
 @pp.route("/search/pagination/")
-def search_pagination():
+def search_results():
+    """Protection Profile search (raw results, pagination + timeline)."""
+
     def callback(**kwargs):
         return url_for(".search", **kwargs)
 
-    res = process_search(request, callback=callback)
-    return render_template("pp/search_pagination.html.jinja2", **res)
+    res = PPBasicSearch.process_search(request, callback=callback)
+    return render_template("pp/search/results.html.jinja2", **res)
 
 
 @pp.route("/<string(length=16):hashid>/")
@@ -194,10 +113,10 @@ def entry(hashid):
             diffs = list(map(load, diffs))
             diff_renders = list(map(lambda x: renderer.render_diff(hashid, doc, x, linkback=False), diffs))
         with sentry_sdk.start_span(op="files", description="Find local files"):
-            local_files = entry_download_files(hashid, current_app.config["DATASET_PATH_CC_DIR"])
+            local_files = entry_download_files(hashid, current_app.config["DATASET_PATH_PP_DIR"])
         name = doc["web_data"]["name"] if doc["web_data"] and doc["web_data"]["name"] else ""
         return render_template(
-            "pp/entry.html.jinja2",
+            "pp/entry/index.html.jinja2",
             profile=doc,
             hashid=hashid,
             certs=certs,
@@ -210,6 +129,26 @@ def entry(hashid):
         )
     else:
         return abort(404)
+
+
+@pp.route("/<string(length=16):hashid>/report.txt")
+def entry_report_txt(hashid):
+    return entry_download_report_txt("pp", hashid, current_app.config["DATASET_PATH_PP_DIR"])
+
+
+@pp.route("/<string(length=16):hashid>/report.pdf")
+def entry_report_pdf(hashid):
+    return entry_download_report_pdf("pp", hashid, current_app.config["DATASET_PATH_PP_DIR"])
+
+
+@pp.route("/<string(length=16):hashid>/profile.txt")
+def entry_profile_txt(hashid):
+    return entry_download_profile_txt("pp", hashid, current_app.config["DATASET_PATH_PP_DIR"])
+
+
+@pp.route("/<string(length=16):hashid>/profile.pdf")
+def entry_profile_pdf(hashid):
+    return entry_download_profile_pdf("pp", hashid, current_app.config["DATASET_PATH_PP_DIR"])
 
 
 @pp.route("/<string(length=16):hashid>/profile.json")
@@ -252,7 +191,7 @@ def entry_name(name):
             return redirect(url_for("pp.entry", hashid=ids[0]["_id"]))
         else:
             docs = list(map(load, mongo.db.pp.find({"_id": {"$in": list(map(itemgetter("_id"), ids))}})))
-            return render_template("pp/disambiguate.html.jinja2", pps=docs, name=name)
+            return render_template("pp/entry/disambiguate.html.jinja2", pps=docs, name=name)
     else:
         return abort(404)
 
@@ -278,4 +217,4 @@ def sitemap_urls():
     yield "pp.search", {}
     yield "pp.rand", {}
     for doc in mongo.db.pp.find({}, {"_id": 1}):
-        yield "pp.entry", {"hashid": doc["_id"]}
+        yield "pp.entry", {"hashid": doc["_id"]}, None, None, 0.8
