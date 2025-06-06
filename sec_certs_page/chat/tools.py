2005bc8cabafca4a0495d4f2db479770018fec59
J08nY <johny@neuromancer.sk>
2025-06-06 16:30:27 +0200

    Enhance WebUI tool.

--- sec_certs_page/chat/tools.py
+++ sec_certs_page/chat/tools.py
@@ -16,6 +16,83 @@ from bs4 import BeautifulSoup
 # Note, this file is a "tool" for the Open WebUI service, it is not a part of this application.
 
 
+def search(query: str, scheme: Literal["cc"] | Literal["fips"], page: int, sort, status) -> str:
+    url = f"https://sec-certs.org/{scheme}/mergedsearch/"
+    per_page = 100
+    if scheme not in ["cc", "fips"]:
+        return f"Error: Invalid scheme '{scheme}'. Valid options are 'cc' for Common Criteria or 'fips' for FIPS 140."
+    params = {
+        "searchType": "by-name",
+        "q": query,
+        "page": page,
+        "per_page": per_page,
+        "sort": sort,
+        "status": status,
+    }
+    headers = {"User-Agent": "WebUI"}
+    resp = requests.get(url, params=params, headers=headers)  # type: ignore
+    if resp.status_code != 200:
+        return f"Error: Unable to fetch data from {url}. Status code: {resp.status_code}"
+    soup = BeautifulSoup(resp.text, "lxml")
+    results = soup.find_all("tr", class_="search-result")
+    if not results:
+        return "No certificates found."
+    output = []
+    for result in results:
+        category = result.find("span", class_="result-category").find("span")["title"]
+        href = result.find("a", class_="result-link")["href"]
+        link = "https://sec-certs.org" + href
+        dgst = href.split("/")[-2]
+        id = result.find("span", class_="result-id").text.strip()
+        name = result.find("span", class_="result-name").text.strip()
+        status = result.find("span", class_="result-status").text.strip()
+        cert_date = result.find("span", class_="result-cert-date").text.strip()
+        archive_date = result.find("span", class_="result-archive-date").text.strip()
+        output.append(
+            {
+                "name": name,
+                "category": category,
+                "id": id,
+                "dgst": dgst,
+                "link": link,
+                "status": status,
+                "certification_date": cert_date,
+                "archival_date": archive_date,
+            }
+        )
+    info = soup.find("div", class_="pagination-page-info")
+    if info:
+        results_info = info.text.strip()
+    else:
+        results_info = "No results information available."
+    pages = soup.find("ul", class_="pagination")
+    if pages:
+        min_page = None
+        max_page = None
+        for li in pages.find_all("li"):
+            try:
+                tpage = int(li.text.strip())
+            except ValueError:
+                continue
+            if min_page is None or tpage < min_page:
+                min_page = tpage
+            if max_page is None or tpage > max_page:
+                max_page = tpage
+        page_info = f"Page {page} of total {max_page} pages."
+    else:
+        page_info = "No pagination information available."
+    result = {
+        "query": query,
+        "scheme": scheme,
+        "page": page,
+        "per_page": per_page,
+        "results_info": results_info,
+        "page_info": page_info,
+        "results": output,
+    }
+    return json.dumps(result, indent=2, ensure_ascii=False)
+
+
 class Tools:
     def __init__(self):
         pass
@@ -65,84 +142,6 @@ class Tools:
             return f"Error: Unable to fetch data from {url}. Status code: {resp.status_code}"
         return resp.content.decode()
 
-    def _search(self, query: str, scheme: Literal["cc"] | Literal["fips"], page: int, sort, status) -> str:
-        url = f"https://sec-certs.org/{scheme}/mergedsearch/"
-        per_page = 100
-        if scheme not in ["cc", "fips"]:
-            return (
-                f"Error: Invalid scheme '{scheme}'. Valid options are 'cc' for Common Criteria or 'fips' for FIPS 140."
-            )
-        params = {
-            "searchType": "by-name",
-            "q": query,
-            "page": page,
-            "per_page": per_page,
-            "sort": sort,
-            "status": status,
-        }
-        headers = {"User-Agent": "WebUI"}
-        resp = requests.get(url, params=params, headers=headers)  # type: ignore
-        if resp.status_code != 200:
-            return f"Error: Unable to fetch data from {url}. Status code: {resp.status_code}"
-        soup = BeautifulSoup(resp.text, "lxml")
-        results = soup.find_all("tr", class_="search-result")
-        if not results:
-            return "No certificates found."
-        output = []
-        for result in results:
-            category = result.find("span", class_="result-category").find("span")["title"]
-            href = result.find("a", class_="result-link")["href"]
-            link = "https://sec-certs.org" + href
-            dgst = href.split("/")[-2]
-            id = result.find("span", class_="result-id").text.strip()
-            name = result.find("span", class_="result-name").text.strip()
-            status = result.find("span", class_="result-status").text.strip()
-            cert_date = result.find("span", class_="result-cert-date").text.strip()
-            archive_date = result.find("span", class_="result-archive-date").text.strip()
-            output.append(
-                {
-                    "name": name,
-                    "category": category,
-                    "id": id,
-                    "dgst": dgst,
-                    "link": link,
-                    "status": status,
-                    "certification_date": cert_date,
-                    "archival_date": archive_date,
-                }
-            )
-        info = soup.find("div", class_="pagination-page-info")
-        if info:
-            results_info = info.text.strip()
-        else:
-            results_info = "No results information available."
-        pages = soup.find("ul", class_="pagination")
-        if pages:
-            min_page = None
-            max_page = None
-            for li in pages.find_all("li"):
-                try:
-                    tpage = int(li.text.strip())
-                except ValueError:
-                    continue
-                if min_page is None or tpage < min_page:
-                    min_page = tpage
-                if max_page is None or tpage > max_page:
-                    max_page = tpage
-            page_info = f"Page {page} of total {max_page} pages."
-        else:
-            page_info = "No pagination information available."
-        result = {
-            "query": query,
-            "scheme": scheme,
-            "page": page,
-            "per_page": per_page,
-            "results_info": results_info,
-            "page_info": page_info,
-            "results": output,
-        }
-        return json.dumps(result, indent=2, ensure_ascii=False)
-
     def cc_search(
         self,
         query: str,
@@ -163,7 +162,7 @@ class Tools:
             return f"Error: Invalid sort parameter '{sort}'. Valid options are 'match', 'name', 'cert_date', or 'archive_date'."
         if status not in ["any", "active", "archived"]:
             return f"Error: Invalid status parameter '{status}'. Valid options are 'any', 'active', or 'archived'."
-        return self._search(query, "cc", page, sort, status)
+        return search(query, "cc", page, sort, status)
 
     def fips_search(
         self,
@@ -193,7 +192,7 @@ class Tools:
             return f"Error: Invalid sort parameter '{sort}'. Valid options are 'match', 'number', 'first_cert_date', 'last_cert_date', 'sunset_date', 'level', or 'vendor'."
         if status not in ["Any", "Active", "Historical", "Revoked"]:
             return f"Error: Invalid status parameter '{status}'. Valid options are 'Any', 'Active', 'Historical', or 'Revoked'."
-        return self._search(query, "fips", page, sort, status)
+        return search(query, "fips", page, sort, status)
 
 
 if __name__ == "__main__":
