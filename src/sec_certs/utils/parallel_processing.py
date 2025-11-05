801b6eaf8a81059ed2a73170de94f4f8f02b72d7
Jakub Borsky <79481056+jborsky@users.noreply.github.com>
2025-11-05 16:10:19 +0100

    feat: track individual items in progress bar during batched parallel processing

--- src/sec_certs/utils/parallel_processing.py
+++ src/sec_certs/utils/parallel_processing.py
@@ -10,6 +10,24 @@ from sec_certs.configuration import config
 from sec_certs.utils.tqdm import tqdm
 
 
+def get_batches(items: list, max_workers: int, min_batch_size: int) -> tuple[list[list], list]:
+    max_batches = max(1, len(items) // min_batch_size)
+    actual_workers = min(max_batches, max_workers)
+    batch_size = len(items) // actual_workers
+    remainder = len(items) % actual_workers
+
+    batches = []
+    batch_sizes = []
+    start = 0
+    for i in range(actual_workers):
+        size = batch_size + (1 if i < remainder else 0)
+        batches.append(items[start : start + size])
+        batch_sizes.append(size)
+        start += size
+
+    return batches, batch_sizes
+
+
 def process_parallel(
     func: Callable,
     items: Iterable,
@@ -29,19 +47,8 @@ def process_parallel(
     if batching:
         assert min_batch_size > 1
         unpack = False
-        items = list(items)
-        max_batches = max(1, len(items) // min_batch_size)
-        actual_workers = min(max_batches, max_workers)
-        batch_size = len(items) // actual_workers
-        remainder = len(items) % actual_workers
-
-        batches = []
-        start = 0
-        for i in range(actual_workers):
-            size = batch_size + (1 if i < remainder else 0)
-            batches.append(items[start : start + size])
-            start += size
-
+        batches, batch_sizes = get_batches(list(items), max_workers, min_batch_size)
+        actual_workers = len(batches)
         items = batches
 
     pool: Pool | ThreadPool = ThreadPool(actual_workers) if use_threading else Pool(actual_workers)
@@ -52,12 +59,17 @@ def process_parallel(
     )
 
     if progress_bar is True and items:
-        bar = tqdm(total=len(results), desc=progress_bar_desc)
+        total = sum(batch_sizes) if batching else len(results)
+        bar = tqdm(total=total, desc=progress_bar_desc)
         while not all(all_done := [x.ready() for x in results]):
-            done_count = len(list(filter(lambda x: x, all_done)))
+            if batching:
+                done_count = sum(batch_sizes[i] for i, ready in enumerate(all_done) if ready)
+            else:
+                done_count = len(list(filter(lambda x: x, all_done)))
             bar.update(done_count - bar.n)
             time.sleep(1)
-        bar.update(len(results) - bar.n)
+
+        bar.update(total - bar.n)
         bar.close()
 
     pool.close()
