95e3174ba4cfd514ee4f391efd1eae856478be7d
Jakub Borsky <79481056+jborsky@users.noreply.github.com>
2025-11-05 13:29:36 +0100

    feat: add batching option to parallel processing

--- src/sec_certs/utils/parallel_processing.py
+++ src/sec_certs/utils/parallel_processing.py
@@ -14,6 +14,8 @@ def process_parallel(
     func: Callable,
     items: Iterable,
     max_workers: int = config.n_threads,
+    batching: bool = False,
+    min_batch_size: int = 4,
     callback: Callable | None = None,
     use_threading: bool = True,
     progress_bar: bool = True,
@@ -23,7 +25,26 @@ def process_parallel(
     if max_workers == -1:
         max_workers = cpu_count()
 
-    pool: Pool | ThreadPool = ThreadPool(max_workers) if use_threading else Pool(max_workers)
+    actual_workers = max_workers
+    if batching:
+        assert min_batch_size > 1
+        unpack = False
+        items = list(items)
+        max_batches = max(1, len(items) // min_batch_size)
+        actual_workers = min(max_batches, max_workers)
+        batch_size = len(items) // actual_workers
+        remainder = len(items) % actual_workers
+
+        batches = []
+        start = 0
+        for i in range(actual_workers):
+            size = batch_size + (1 if i < remainder else 0)
+            batches.append(items[start : start + size])
+            start += size
+
+        items = batches
+
+    pool: Pool | ThreadPool = ThreadPool(actual_workers) if use_threading else Pool(actual_workers)
     results = (
         [pool.apply_async(func, (*i,), callback=callback) for i in items]
         if unpack
