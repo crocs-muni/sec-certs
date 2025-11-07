677f4f4885d11ff8be09af23eb93ae3a8ebbbca0
Jakub Borsky <79481056+jborsky@users.noreply.github.com>
2025-11-07 15:56:58 +0100

    feat: make docling an optional dependency

--- requirements/compile.sh
+++ requirements/compile.sh
@@ -6,4 +6,5 @@ pip-compile --upgrade --no-header -o requirements.txt ./../pyproject.toml
 pip-compile --upgrade --no-header --extra dev -o dev_requirements.txt ./../pyproject.toml
 pip-compile --upgrade --no-header --extra test -o test_requirements.txt ./../pyproject.toml
 pip-compile --upgrade --no-header --extra nlp -o nlp_requirements.txt ./../pyproject.toml
-pip-compile --upgrade --no-header --extra dev --extra test --extra nlp -o all_requirements.txt ./../pyproject.toml
+pip-compile --upgrade --no-header --extra docling -o docling_requirements.txt ./../pyproject.toml
+pip-compile --upgrade --no-header --extra dev --extra test --extra nlp --extra docling -o all_requirements.txt ./../pyproject.toml
