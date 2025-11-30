529e17ccbf3ef717b97bfabcbca3f238ee5cb5c6
Nikola Davidova <ndavidov@redhat.com>
2025-11-30 19:23:48 +0100

    Update fips certificate with br1 metadata, implement extraction

--- src/sec_certs/sample/fips.py
+++ src/sec_certs/sample/fips.py
@@ -15,6 +15,11 @@ import requests
 from bs4 import BeautifulSoup, Tag
 
 from sec_certs import constants
+from sec_certs.br1.chapter_parsing.mapper import extract_chapters_from_text
+from sec_certs.br1.chapter_parsing.validator import validate_chapters
+from sec_certs.br1.config.constants import ERROR_ACCEPT as BR1_MAX_ERROR
+from sec_certs.br1.table_parsing.model.br1_tables import BR1TablesClass
+from sec_certs.br1.table_parsing.parser import parse_tables
 from sec_certs.cert_rules import FIPS_ALGS_IN_TABLE, fips_rules
 from sec_certs.configuration import config
 from sec_certs.converter import PDFConverter
@@ -469,6 +474,9 @@ class FIPSCertificate(
         module_processed_references: References = field(default_factory=References)
         direct_transitive_cves: set[str] | None = field(default=None)
         indirect_transitive_cves: set[str] | None = field(default=None)
+        is_br1_format: bool = field(default=False)
+        br1_deviations: int = field(default=0)
+        br1_tables: BR1TablesClass | None = field(default=None)
 
         @property
         def algorithm_numbers(self) -> set[str]:
@@ -644,6 +652,26 @@ class FIPSCertificate(
             cert.state.policy_extract_ok = False
         else:
             cert.pdf_data.keywords = keywords
+        return cert
+
+    @staticmethod
+    def extract_br1_metadata(cert: FIPSCertificate) -> FIPSCertificate:
+        """
+        Extract br1 chapters and tables from the document
+        """
+        with open(cert.state.policy_txt_path) as f:
+            file_text = f.read()
+            chapters = extract_chapters_from_text(file_text)
+
+        error, _ = validate_chapters(chapters)
+        cert.heuristics.br1_deviations = error
+
+        is_br1 = error <= BR1_MAX_ERROR
+        cert.heuristics.is_br1_format = is_br1
+
+        if is_br1:
+            cert.heuristics.br1_tables = parse_tables(chapters)
+
         return cert
 
     @staticmethod
