f0a5a5cef7649d8aecaadd066d4670aaae8314d1
David Valecký <D.Valecky@gmail.com>
2025-12-01 07:24:41 +0100

    feat(dashboard): Hide the functionality behind a dashboard role

--- sec_certs_page/common/permissions.py
+++ sec_certs_page/common/permissions.py
@@ -3,3 +3,5 @@ from flask_principal import Permission, RoleNeed
 admin_permission = Permission(RoleNeed("admin"))
 
 chat_permission = Permission(RoleNeed("chat"), RoleNeed("admin"))
+
+dashboard_permission = Permission(RoleNeed("dashboard"), RoleNeed("admin"))
