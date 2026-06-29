624552da6e10cc1296ee821aa334b6098c47821b
Martin Mikulica <m.mikulica@seznam.cz>
2026-06-29 02:41:41 +0200

    x.509 dot fix

--- sec_certs_page/jinja.py
+++ sec_certs_page/jinja.py
@@ -187,7 +187,7 @@ def standard_url(standard):
         path = f"{base}-{sub}" if sub else base
         return f"https://csrc.nist.gov/pubs/fips/{path}/final"
 
-    if re.match(r"X\.?509", s, re.IGNORECASE):
+    if re.match(r"X[.．]?509", s, re.IGNORECASE):
         return "https://www.itu.int/rec/T-REC-X.509"
 
     return None
