7dd48e5bb1efd29cef41e92eebb2647a1ad456c7
Martin Mikulica <m.mikulica@seznam.cz>
2026-06-29 02:55:11 +0200

    add ICAO standard link

--- sec_certs_page/jinja.py
+++ sec_certs_page/jinja.py
@@ -194,6 +194,9 @@ def standard_url(standard):
     if re.match(r"X[.．]?509", s, re.IGNORECASE):
         return "https://www.itu.int/rec/T-REC-X.509"
 
+    if re.match(r"ICAO", s, re.IGNORECASE):
+        return "https://www.icao.int/publications/doc-series"
+
     return None
 
 
