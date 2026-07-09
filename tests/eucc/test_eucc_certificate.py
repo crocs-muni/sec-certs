2eb80a81b1dab81c2e6b866ad1b6a1d8abb05b0f
Tadeáš Kachyňa <t.kachyna@email.cz>
2026-07-09 19:08:44 +0200

    Add tests

--- tests/eucc/test_eucc_certificate.py
+++ tests/eucc/test_eucc_certificate.py
@@ -89,6 +89,8 @@ def test_parse_package(input_text, expected_output):
         ("EUCC-3110-2025-08-2500052-01", "NL"),
         ("EUCC-3110-2025-0002500052-00001", "NL"),
         ("EUCC-3090-2025-0000000006-00000", "FR"),
+        ("EUCC-3135-2026-0000000001-00000", "SE"),
+        ("EUCC-3100-2026-0007001701-00000", "NL")
     ],
 )
 def test_get_scheme_from_cert_id(cert_id, expected_output):
